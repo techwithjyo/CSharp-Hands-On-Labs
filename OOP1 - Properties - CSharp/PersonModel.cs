b06c031d49e07b714a7faa7126df3e1fb5aaba5a
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2021-10-16 19:59:43 +0530

    Adding new project - Properties in CSharp

---
+++ OOP1 - Properties - CSharp/PersonModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1___Properties___CSharp
+{
+    class PersonModel
+    {
+        public string FirstName { get; set; } // => Auto Property
+        public string LastName { get; set; }
+        //public int Age { get; set; }
+
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value >= 0 && value < 126)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age needs to be in valid number range..");
+                }
+            }
+        }
+
+        private string _ssn;
+
+        public string SSN
+        {
+            get
+            {
+                string output = "***-**-" + _ssn.Substring(_ssn.Length - 4);
+
+                return output;
+            }
+            set { _ssn = value; }
+        }
+
+        private string _fullName;
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+            private set
+            {
+                _fullName = value;
+            }
+        }
+
+
+    }
+}
---
+++ OOP1 - Properties - CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1___Properties___CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "Jyotirmoy";
+            person.LastName = "Maschatak";
+            person.Age = 40;
+            person.SSN = "123-45-6789";
+            //person.FullName = "Jyotirmoy Maschatak"; //as the property modifier is private
+
+            Console.WriteLine(person.SSN);
+
+            Console.WriteLine(person.FullName);
+
+
+            Console.ReadLine();
+        }
+    }
+}
