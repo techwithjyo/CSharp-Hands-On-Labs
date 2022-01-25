a1fd6a4c145ef2858cc7b1d95fd97adf68838413
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-01-25 23:08:57 +0530

    Adding SQL Data Adapter example in Project

--- DotNet - ADO DotNet/Program.cs
+++ DotNet - ADO DotNet/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace DotNet___ADO_DotNet
 {
@@ -11,29 +12,53 @@ namespace DotNet___ADO_DotNet
     {
         static void Main(string[] args)
         {
+            #region DataReader Section
+            ////Step 1 : Create Connection Object
+            //SqlConnection sqlcon = new SqlConnection("Server=localhost;Database=dev1;Trusted_Connection=True;");
+            //sqlcon.Open();
+
+            ////Step 2 : Command (SQL)
+            //SqlCommand sqlCommand = new SqlCommand("select * from Persons",sqlcon);
+
+
+            ////SqlCommand sqlCommand1 = new SqlCommand();
+            ////sqlCommand1.CommandText = "spSelectPersons";
+            ////sqlCommand1.CommandType = System.Data.CommandType.StoredProcedure;
+            ////sqlCommand1.Connection = sqlcon;
+
+            ////Step 3 : Get the data from Datareader object to browse and fetch the data
+            //SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            //while(sqlDataReader.Read())
+            //{
+            //    //Display Customer ID and Name
+            //    Console.WriteLine(sqlDataReader["PersonID"]+" -- "+ sqlDataReader["FirstName"] + " "+ sqlDataReader["LastName"]);
+            //}
+
+            //sqlcon.Close();
+
+            #endregion
+            #region DataAdapter
             //Step 1 : Create Connection Object
             SqlConnection sqlcon = new SqlConnection("Server=localhost;Database=dev1;Trusted_Connection=True;");
             sqlcon.Open();
 
             //Step 2 : Command (SQL)
-            SqlCommand sqlCommand = new SqlCommand("select * from Persons",sqlcon);
+            SqlCommand sqlCommand = new SqlCommand("select * from Persons", sqlcon);
 
 
-            //SqlCommand sqlCommand1 = new SqlCommand();
-            //sqlCommand1.CommandText = "spSelectPersons";
-            //sqlCommand1.CommandType = System.Data.CommandType.StoredProcedure;
-            //sqlCommand1.Connection = sqlcon;
+            //Step 3 : Get the data from Datasets
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataSet ds = new DataSet();
+            sqlDataAdapter.Fill(ds);
 
-            //Step 3 : Get the data from Datareader object to browse and fetch the data
-            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-
-            while(sqlDataReader.Read())
+            foreach(DataRow dr in ds.Tables[0].Rows)
             {
-                //Display Customer ID and Name
-                Console.WriteLine(sqlDataReader["PersonID"]+" -- "+ sqlDataReader["FirstName"] + " "+ sqlDataReader["LastName"]);
+                Console.WriteLine(dr["PersonID"]+" -- " + dr["FirstName"] + " " + dr["LastName"]);
             }
 
             sqlcon.Close();
+            #endregion
         }
     }
 }
