3b52e980fd6e1e94e119819a8e3e067a788863bf
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-01-25 19:48:52 +0530

    Adding code for Data Reader (ADO .Net)

---
+++ DotNet - ADO DotNet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DotNet___ADO_DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1 : Create Connection Object
+            SqlConnection sqlcon = new SqlConnection("Server=localhost;Database=dev1;Trusted_Connection=True;");
+            sqlcon.Open();
+
+            //Step 2 : Command (SQL)
+            SqlCommand sqlCommand = new SqlCommand("select * from Persons",sqlcon);
+
+
+            //SqlCommand sqlCommand1 = new SqlCommand();
+            //sqlCommand1.CommandText = "spSelectPersons";
+            //sqlCommand1.CommandType = System.Data.CommandType.StoredProcedure;
+            //sqlCommand1.Connection = sqlcon;
+
+            //Step 3 : Get the data from Datareader object to browse and fetch the data
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while(sqlDataReader.Read())
+            {
+                //Display Customer ID and Name
+                Console.WriteLine(sqlDataReader["PersonID"]+" -- "+ sqlDataReader["FirstName"] + " "+ sqlDataReader["LastName"]);
+            }
+
+            sqlcon.Close();
+        }
+    }
+}
