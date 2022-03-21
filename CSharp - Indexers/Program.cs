c13e953bf3a19d4b7f6c8999c4771521c5c19893
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-03-21 18:13:28 +0530

    Adding project - Indexers and Properties

---
+++ CSharp - Indexers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp___Indexers
+{
+    class IndexerClass
+    {
+        private string[] cities = new string[10];
+
+        public string this[int i]
+        {
+            get 
+            {
+                return cities[i];
+            }
+            set 
+            {
+                cities[i] = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexerClass citiesObject = new IndexerClass();
+            citiesObject[1] = "Kolkata";
+            citiesObject[2] = "Hyderabad";
+            citiesObject[3] = "Bangalore";
+            citiesObject[4] = "Pune";
+            citiesObject[5] = "Chennai";
+
+            for(int i=0;i<10; i++)
+            {
+                Console.WriteLine(citiesObject[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp - Properties Dotnettricks/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp___Properties_Dotnettricks
+{
+    class User
+    {
+        public string publicProperty;
+        private string firstname;
+        
+        public string FirstName
+        { //full implementation
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string LastName { get; set; } //short implementation
+
+        public string FullName
+        {
+            //set is blocked, that means it is a readonly property
+            get
+            {
+                return firstname +" "+ LastName;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User usr = new User();
+            usr.publicProperty = "Public Property";
+
+            usr.FirstName = "Jyotirmoy";
+
+            usr.LastName = "Maschatak";
+
+            //usr.FullName = "dasd";
+
+            Console.WriteLine("Public Property- "+usr.publicProperty + " //Declared Property- "+ usr.FirstName+" //LastName- "+usr.LastName);
+            Console.WriteLine("Full Name Private Property without Set - "+usr.FullName);
+            Console.ReadLine();
+        }
+    }
+}
