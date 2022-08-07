1b3cb50f7ab252cd0d0668d8ccd57188ca304f19
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-08-07 22:34:08 +0530

    Adding test project for DSA

---
+++ DSA Array Test/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+
+int[] arr2 = arr1;
+
+arr1[0] = 10;
+
+Console.WriteLine(arr1[0]);
+Console.WriteLine(arr2[0]);
+Console.ReadLine();
