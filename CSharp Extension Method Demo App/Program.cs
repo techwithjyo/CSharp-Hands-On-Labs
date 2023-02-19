9bafabe12bedbc38e54d6fd6fa793ff01485fa86
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2023-02-19 23:13:04 +0530

    Extension Methods demo from Tim

---
+++ CSharp Extension Method Demo App/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Extension_Method_Demo_App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HotelRoomModel room = new HotelRoomModel();
+
+            room.TurnOnAir().SetTemperature(52).OpenShades();
+
+            room.TurnOffAir().CloseShades();
+
+            "Hello World".PrintToConsole();
+            Console.ReadLine();
+        }
+    }
+    public class HotelRoomModel
+    {
+        public int Temperature { get; set; }
+        public bool IsAirRunning { get; set; }
+        public bool AreShadesOpen { get; set; }
+    }
+    public static class ExtensionSamples
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static HotelRoomModel TurnOnAir(this HotelRoomModel room)
+        {
+            room.IsAirRunning = true;
+            return room;
+        }
+        public static HotelRoomModel TurnOffAir(this HotelRoomModel room)
+        {
+            room.IsAirRunning = false;
+            return room;
+        }
+        public static HotelRoomModel SetTemperature(this HotelRoomModel room, int temperature)
+        {
+            room.Temperature = temperature;
+            return room;
+        }
+        public static HotelRoomModel OpenShades(this HotelRoomModel room)
+        {
+            room.AreShadesOpen = true;
+            return room;
+        }
+        public static HotelRoomModel CloseShades(this HotelRoomModel room)
+        {
+            room.AreShadesOpen = false;
+            return room;
+        }
+    }
+}
