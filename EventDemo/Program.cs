0d8ea28ba691cf4550b92596b055309ba46d989a
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2023-03-07 01:09:59 +0530

    Events demo

--- EventDemo/Program.cs
+++ EventDemo/Program.cs
@@ -14,17 +14,48 @@ namespace EventDemo
 
             CollegeClassModel math = new CollegeClassModel("Calculas 101", 2);
 
+            history.EnrollmentFull += CollegeClass_EnrollmentFull; //listner
+
             history.EnrollStudent("Jyotirmoy").PrintToConsole();
             history.EnrollStudent("Person A").PrintToConsole();
             history.EnrollStudent("Person B").PrintToConsole();
             history.EnrollStudent("Person C").PrintToConsole();
             history.EnrollStudent("Person D").PrintToConsole();
+            Console.WriteLine();
+
+            math.EnrollmentFull += CollegeClass_EnrollmentFull;
 
             math.EnrollStudent("Jyotirmoy").PrintToConsole();
             math.EnrollStudent("Person A").PrintToConsole();
             math.EnrollStudent("Person B").PrintToConsole();
 
             Console.ReadLine();
+        }
+
+        private static void CollegeClass_EnrollmentFull(object sender, string e) //Combined method for Math + History
+        {
+            CollegeClassModel model = (CollegeClassModel)sender;
+
+            Console.WriteLine();
+            Console.WriteLine(model.CourseTitle + " is full!!");
+            Console.WriteLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine(e);
+            //Console.WriteLine();
+        }
+        private static void Math_EnrollmentFull(object sender, string e)
+        {
+            Console.WriteLine();
+            Console.WriteLine("The enrollment is full for Math!");
+            Console.WriteLine();
+        }
+
+        private static void History_EnrollmentFull(object sender, string e) //listner
+        {
+            Console.WriteLine();
+            Console.WriteLine("The enrollment is full for History!");
+            Console.WriteLine();
         }
     }
     public static class ConsoleHelpers
@@ -36,6 +67,8 @@ namespace EventDemo
     }
     public class CollegeClassModel
     {
+        public event EventHandler<string> EnrollmentFull; //event handler
+
         List<string> enrolledStudent = new List<string>();
         List<string> waitingList = new List<string>();
 
@@ -53,6 +86,8 @@ namespace EventDemo
             {
                 enrolledStudent.Add(studentName);
                 output = studentName + " was enrolled in " + CourseTitle;
+                if(enrolledStudent.Count == MaximumStudents)
+                    EnrollmentFull?.Invoke(this, CourseTitle + " Enrollment is full.."); //invokation
             }
             else
             {
