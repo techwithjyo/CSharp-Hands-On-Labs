ca54594c0cfa175ebc5a810d3656b8ab992c4080
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2023-05-11 20:12:15 +0530

    Adding some code for Lambda Expression

---
+++ CSharp Lambda Expression MSDocs/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Func<int, int> square = x => x * x;
+//Console.WriteLine(square(10));
+
+//System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+//Console.WriteLine(e);
+
+//Action<string> greet = name =>
+//{
+//    var str = "Hello " + name;
+//    Console.WriteLine(str);
+//};
+
+//greet("xyz");
+
+//Not passing any parameters
+Console.WriteLine("Before Execution!!");
+Action line = () => Console.WriteLine();
+line();
+Console.WriteLine("After Execution!!");
+
+//Passing one parameter
+Action<string> printMessage = (message) => Console.WriteLine(message);
+printMessage("My Message!");
+
+//Passing multiple Parameters
+Func<int, int, bool> checkForEquality = (x, y) => x == y;
+Console.WriteLine(checkForEquality(100, 100));
+
+Console.ReadKey();  
