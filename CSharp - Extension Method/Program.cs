4a34dbe334d70d43189807e7b11944219aad674d
jyotirmoy <jmaschatak1@gmail.com>
2024-08-20 22:52:41 +0530

    Extension method, adding extra code and comments

--- CSharp - Extension Method/Program.cs
+++ CSharp - Extension Method/Program.cs
@@ -8,11 +8,22 @@ namespace CSharp___Extension_Method
 {
     public static class WordExtension
     {
+        //Must be static Class, Must be static method, This keyword is must
+
+        //Extension methods in C# allow you to add new methods to existing types without modifying
+        //the original type or creating a new derived type. They are defined as static methods
+        //in a static class, but they are called as if they were instance methods on the extended type.
         public static int WordCountFromMethod(this string str)
         {
             string[] strArray = str.Split(' ');
             int wordCount = strArray.Length;
             return wordCount;
+        }
+        public static int WordCount(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return 0;
+            string[] words = str.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
         }
     }
     class Program
@@ -22,8 +33,9 @@ namespace CSharp___Extension_Method
             string content = "fafafaf afca af fs f sdf sf sdf sdf sdf sdf sdf s";
 
             int count = content.WordCountFromMethod();
+            int wordCount = content.WordCount();
 
-            Console.WriteLine(count);
+            Console.WriteLine($"Count: {count}, WordCount: {wordCount}");
             Console.ReadLine();
         }
     }
